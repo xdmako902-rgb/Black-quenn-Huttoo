1e2f6f27e0fbc52d743aa5c21432b2ee9a82f5ca
@Mako'Z <xdmako902@gmail.com>
2026-08-15 00:56:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    PREFIX: ".",
+    OWNER_NUMBER: "94766619363",
+    BOT_NAME: "Ｒɪᴋᴀ xᴍᴅ V3",
+    WORK_TYPE: "public",
+
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
+
+    AUTO_REACT: false,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+
+    AUTO_READ_STATUS: true,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
+
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
+    DELETEMSGSENDTO: "owner",
+        
+    NEWSLETTER_JID: "120363428073031350@newsletter",
+    GROUP_INVITE_LINK: "https://chat.whatsapp.com/L3uA3FqcKxU3B2FlMd52Sh",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://i.ibb.co/4nH9Dvch/e90b7cd6667a.jpg"
+};
